28d301da6836d9fb2d7abda11a5951083f25c057
Martin Blomberg <martin.blomberg@outlook.com>
2019-01-10 14:08:18 +0100

    Uppgift4

--- src/Uppgift4.java
+++ src/Uppgift4.java
@@ -2,6 +2,15 @@ public class Uppgift4 {
 
 
     public static void main(String[] args) {
+        int tal1 = Main.getInt();
+        int tal2 = Main.getInt();
+        double mean = meanValue(tal1, tal2);
+        System.out.println(mean);
+    }
 
+    public static double meanValue(int a, int b){
+
+        double m = (a + b) / 2.0;
+        return m;
     }
 }
