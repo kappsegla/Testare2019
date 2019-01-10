79e158aae4b694cd0377583b54af0f50bcdf57e4
Martin Blomberg <martin.blomberg@outlook.com>
2019-01-10 12:22:35 +0100

    Uppgift1 Updated

--- src/Uppgift1.java
+++ src/Uppgift1.java
@@ -10,14 +10,5 @@ public class Uppgift1 {
 
     public static boolean isEven(int talet){
         return talet % 2 == 0;
-//
-//        if( talet % 2 == 0 ){
-//            //Jämnt tal
-//            return true;
-//        }
-//       else{
-//            //Udda tal
-//            return false;
-//        }
     }
 }
