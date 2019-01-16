619205798d88f95a8e593a7559dff7ae0d0f4ffa
Martin Blomberg <martin.blomberg@outlook.com>
2019-01-16 09:58:48 +0100

    Rectangle class

---
+++ src/oop/Main.java
@@ -0,0 +1,23 @@
+package oop;
+
+import org.w3c.dom.css.Rect;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Rectangle rect = new Rectangle();
+        Rectangle rect2 = new Rectangle();
+
+        rect.setHeight(10);
+        rect2.setHeight(20);
+
+        int i = rect.getHeight();
+
+        System.out.println(i);
+        System.out.println(rect2.getHeight());
+
+        System.out.println( rect.getWidth() );
+
+    }
+}
---
+++ src/oop/Rectangle.java
@@ -0,0 +1,23 @@
+package oop;
+
+public class Rectangle {
+
+    private int width;
+    private int height;
+
+    public void setWidth(int w){
+        width = w;
+    }
+
+    public void setHeight(int h){
+        height = h;
+    }
+
+    public int getWidth(){
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
