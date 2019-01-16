b34701ca45f80f0a8e420fc390ce57594da7da68
Martin Blomberg <martin.blomberg@outlook.com>
2019-01-16 15:07:13 +0100

    Point

--- src/oop/Main.java
+++ src/oop/Main.java
@@ -4,19 +4,39 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Teacher teacher = new Teacher("Martin", "555-123456");
+        Point point1 = new Point(0.0,0.0);
+        Point point2 = new Point(3.0,0.0);
 
-        Course course = new Course("Java", teacher ,
-                new Adress("Göteborg","Maskingatan",5),
-                90, 12);
+        Point point3 = new Point(point2);
 
-        Course course2 = new Course("Testning", teacher ,
-                new Adress("Göteborg","Maskingatan",5),
-                90, 12);
-        
-        System.out.println(course.getTeacher().getName());
+        point3.setX(4.0);
+        System.out.println(point2);
+        System.out.println(point3);
 
-        System.out.println( course );
+
+
+        //double d = point1.distanceTo(point2);
+
+        //System.out.println( d );
+
+
+
+//        Teacher teacher = new Teacher("Martin", "555-123456");
+//
+//        Course course = new Course("Java", teacher ,
+//                new Adress("Göteborg","Maskingatan",5),
+//                90, 12);
+//
+//        Course course2 = new Course("Testning", teacher ,
+//                new Adress("Göteborg","Maskingatan",5),
+//                90, 12);
+//
+//
+
+        //System.out.println(course.getTeacher().getName());
+
+        //Use toString for printing course info
+  //      System.out.println( course );
 
 
 
