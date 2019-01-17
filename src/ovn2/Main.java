da14861b30836998a50c321f05f22ffa1dfc72d1
Martin Blomberg <martin.blomberg@outlook.com>
2019-01-17 11:25:15 +0100

    Ovning 2 Uppgift 1

---
+++ src/ovn2/Main.java
@@ -0,0 +1,15 @@
+package ovn2;
+
+import oop.Adress;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        House myHouse = new House(120,1,1000000,1995,new Adress("Skogen","Lingonstigen", 1));
+
+        System.out.println(myHouse);
+
+    }
+}
