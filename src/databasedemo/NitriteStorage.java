3433753bff8a2e18ea3c87960b4517192ad69daf
Martin Blomberg <martin.blomberg@outlook.com>
2019-02-08 12:21:35 +0100

    NitriteStorage

--- src/databasedemo/NitriteStorage.java
+++ src/databasedemo/NitriteStorage.java
@@ -1,5 +1,8 @@
 package databasedemo;
 
+//Uses org.dizitart:nitrite library
+//https://www.dizitart.org/nitrite-database/#what-is-nitrite
+
 public class NitriteStorage {
 
     public NitriteStorage() {
